e35738d8e18e17a32a1d5da2c8ce7d1415c0a37c
Pham Huu Hien <phhien203@gmail.com>
2023-08-30 20:19:26 +0700

    redirect to the first matched server if any

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,3 +1,24 @@
+import { redirect } from 'next/navigation'
+
+import { prisma } from '@/lib/db'
+import { initialProfile } from '@/lib/initial-profile'
+
 export default async function SetupPage() {
+  const profile = await initialProfile()
+
+  const server = await prisma.server.findFirst({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  })
+
+  if (server) {
+    return redirect(`/servers/${server.id}`)
+  }
+
   return <div>Setup a server</div>
 }
