7e4e00ffea5e6467ce4725f99824f751c4f93b08
Pham Huu Hien <phhien203@gmail.com>
2023-09-06 21:52:20 +0700

    build members dialog p1

--- components/modals/members-modal.tsx
+++ components/modals/members-modal.tsx
@@ -1,95 +1,46 @@
 'use client'
 
-import { CheckIcon, CopyIcon, RefreshCwIcon } from 'lucide-react'
 import React from 'react'
 
-import { Button } from '@/components/ui/button'
 import {
   Dialog,
   DialogContent,
+  DialogDescription,
   DialogHeader,
   DialogTitle,
 } from '@/components/ui/dialog'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
+import { ScrollArea } from '@/components/ui/scroll-area'
 import { useModal } from '@/hooks/use-modal-store'
-import { useOrigin } from '@/hooks/use-origin'
-import axios from 'axios'
+import { ServerWithMembersWithProfiles } from '@/types'
 
 export default function MembersModal() {
-  const origin = useOrigin()
   const { isOpen, onClose, onOpen, type, data } = useModal()
-  const { server } = data
+  const { server } = data as { server: ServerWithMembersWithProfiles }
 
-  const [copied, setCopied] = React.useState(false)
   const [isLoading, setIsLoading] = React.useState(false)
 
   const isModalOpen = isOpen && type === 'members'
-  const inviteUrl = `${origin}/invite/${server?.inviteCode}`
-
-  const onCopy = () => {
-    navigator.clipboard.writeText(inviteUrl)
-    setCopied(true)
-
-    setTimeout(() => {
-      setCopied(false)
-    }, 3000)
-  }
-
-  const onGenerateNewInviteLink = async () => {
-    try {
-      setIsLoading(true)
-
-      const res = await axios.patch(`/api/servers/${server?.id}/invite-code`)
-
-      onOpen('invite', { server: res.data })
-    } catch (error) {
-      console.log(error)
-    } finally {
-      setIsLoading(false)
-    }
-  }
 
   return (
     <Dialog open={isModalOpen} onOpenChange={onClose}>
       <DialogContent className="overflow-hidden bg-white p-0 text-black">
         <DialogHeader className="px-6 pt-8">
           <DialogTitle className="text-center text-2xl font-bold">
-            Invite Friends
+            Manage Members
           </DialogTitle>
+
+          <DialogDescription className="text-center text-zinc-500">
+            {server.members?.length} Member(s)
+          </DialogDescription>
         </DialogHeader>
 
-        <div className="p-6">
-          <Label className="text-xs font-bold uppercase text-zinc-500 dark:text-secondary/70">
-            Server invite link
-          </Label>
-
-          <div className="mt-2 flex items-center gap-x-2">
-            <Input
-              className="border-0 bg-zinc-300/50 text-black focus-visible:ring-0 focus-visible:ring-offset-0"
-              value={inviteUrl}
-              disabled={isLoading}
-            />
-            <Button size="icon" disabled={isLoading} onClick={onCopy}>
-              {copied ? (
-                <CheckIcon className="h-4 w-4" />
-              ) : (
-                <CopyIcon className="h-4 w-4" />
-              )}
-            </Button>
-          </div>
-
-          <Button
-            size="sm"
-            variant="link"
-            className="mt-4 text-xs text-zinc-500"
-            disabled={isLoading}
-            onClick={onGenerateNewInviteLink}
-          >
-            Generate a new link
-            <RefreshCwIcon className="ml-2 h-4 w-4" />
-          </Button>
-        </div>
+        <ScrollArea className="mt-8 max-h-[420px] pr-6">
+          {server.members?.map((member) => (
+            <div key={member.id} className="mb-6 flex items-center gap-x-2">
+              {member.profile.name}
+            </div>
+          ))}
+        </ScrollArea>
       </DialogContent>
     </Dialog>
   )
