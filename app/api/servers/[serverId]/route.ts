2d48aad099a26094898003141e26f79cf467641b
Pham Huu Hien <phhien203@gmail.com>
2023-09-08 22:34:38 +0700

    build api to handle delete server

--- app/api/servers/[serverId]/route.ts
+++ app/api/servers/[serverId]/route.ts
@@ -3,6 +3,36 @@ import { NextResponse } from 'next/server'
 import { currentProfile } from '@/lib/current-profile'
 import { prisma } from '@/lib/db'
 
+export async function DELETE(
+  _req: Request,
+  { params }: { params: { serverId: string } },
+) {
+  try {
+    if (!params.serverId) {
+      return new NextResponse('Server id missing', { status: 400 })
+    }
+
+    const profile = await currentProfile()
+
+    if (!profile) {
+      return new NextResponse('Unauthorized', { status: 401 })
+    }
+
+    const server = await prisma.server.delete({
+      where: {
+        id: params.serverId,
+        profileId: profile.id,
+      },
+    })
+
+    return NextResponse.json(server)
+  } catch (error) {
+    console.error('[SERVER_ID_DELETE]', error)
+    return new NextResponse('Internal error', { status: 500 })
+  }
+}
+
+
 export async function PATCH(
   req: Request,
   { params }: { params: { serverId: string } },
