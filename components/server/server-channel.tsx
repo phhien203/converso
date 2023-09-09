e15deddc9ad5ae3faab41f251443c385921f4708
Pham Huu Hien <phhien203@gmail.com>
2023-09-09 16:11:56 +0700

    handle click on member and channel

--- components/server/server-channel.tsx
+++ components/server/server-channel.tsx
@@ -32,12 +32,17 @@ export default function ServerChannel({ channel, server, role }: Props) {
   const { onOpen } = useModal()
   const Icon = iconMap[channel.type]
 
+  const onClick = () => {
+    router.push(`/servers/${server.id}/channels/${channel.id}`)
+  }
+
   return (
     <button
       className={cn(
         'group mb-1 flex w-full items-center gap-x-2 rounded-md px-2 py-2 transition hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50',
         params?.channelId === channel.id && 'bg-zinc-700/20 dark:bg-zinc-700',
       )}
+      onClick={onClick}
     >
       <Icon className="h-5 w-5 flex-shrink-0 text-zinc-500 dark:text-zinc-400" />
 
@@ -57,14 +62,20 @@ export default function ServerChannel({ channel, server, role }: Props) {
             <ActionTooltip label="Edit">
               <EditIcon
                 className="hidden h-4 w-4 text-zinc-500 transition hover:text-zinc-600 group-hover:block dark:text-zinc-400 dark:hover:text-zinc-300"
-                onClick={() => onOpen('editChannel', { server, channel })}
+                onClick={(e) => {
+                  e.stopPropagation()
+                  onOpen('editChannel', { server, channel })
+                }}
               />
             </ActionTooltip>
 
             <ActionTooltip label="Delete">
               <TrashIcon
                 className="hidden h-4 w-4 text-zinc-500 transition hover:text-zinc-600 group-hover:block dark:text-zinc-400 dark:hover:text-zinc-300"
-                onClick={() => onOpen('deleteChannel', { server, channel })}
+                onClick={(e) => {
+                  e.stopPropagation()
+                  onOpen('deleteChannel', { server, channel })
+                }}
               />
             </ActionTooltip>
           </div>
--- components/server/server-member.tsx
+++ components/server/server-member.tsx
@@ -25,12 +25,17 @@ export default function ServerMember({ member, server }: Props) {
   const router = useRouter()
   const params = useParams()
 
+  const onClick = () => {
+    router.push(`/servers/${server.id}/conversations/${member.id}`)
+  }
+
   return (
     <button
       className={cn(
         'group mb-1 flex w-full items-center gap-x-2 rounded-md px-2 py-2 transition hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50',
         params?.memberId === member.id && 'bg-zinc-700/20 dark:bg-zinc-700',
       )}
+      onClick={onClick}
     >
       <UserAvatar
         src={member.profile.imageUrl}
@@ -40,7 +45,7 @@ export default function ServerMember({ member, server }: Props) {
       <p
         className={cn(
           'text-sm font-semibold text-zinc-500 transition group-hover:text-zinc-600 dark:text-zinc-400 dark:group-hover:text-zinc-300',
-          member.profileId === server.profileId &&
+          member.id === params.memberId &&
             'text-primary dark:text-zinc-200 dark:group-hover:text-white',
         )}
       >
