8624ee0874665fad30353946573c8abb0d589f5c
Pham Huu Hien <phhien203@gmail.com>
2023-09-09 21:04:38 +0700

    build ui for chat input

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,4 +1,5 @@
 import ChatHeader from '@/components/chat/chat-header'
+import ChatInput from '@/components/chat/chat-input'
 import { currentProfile } from '@/lib/current-profile'
 import { prisma } from '@/lib/db'
 import { redirectToSignIn } from '@clerk/nextjs'
@@ -38,6 +39,15 @@ export default async function ChannelIdPage({
         type="channel"
         name={channel.name}
         serverId={channel.serverId}
+      />
+
+      <div className="flex-1">Future Messages</div>
+
+      <ChatInput
+        name={channel.name}
+        type="channel"
+        apiUrl="/api/socket/messages"
+        query={{ channelId: channel.id, serverId: channel.serverId }}
       />
     </div>
   )
