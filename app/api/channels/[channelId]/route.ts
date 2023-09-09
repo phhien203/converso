a6ac670e54d18a3a8b01219994f752501969a9e2
Pham Huu Hien <phhien203@gmail.com>
2023-09-09 15:56:15 +0700

    add api to edit channel

--- app/api/channels/[channelId]/route.ts
+++ app/api/channels/[channelId]/route.ts
@@ -55,3 +55,74 @@ export async function DELETE(
     return new NextResponse('Internal error', { status: 500 })
   }
 }
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { channelId: string } },
+) {
+  try {
+    const { searchParams } = new URL(req.url)
+    const serverId = searchParams.get('serverId')
+
+    if (!serverId) {
+      return new NextResponse('Server id missing', { status: 400 })
+    }
+
+    if (!params.channelId) {
+      return new NextResponse('Channel id missing', { status: 400 })
+    }
+
+    const { name, type } = await req.json()
+
+    if (!name || !type) {
+      return new NextResponse('name or type is missing', { status: 400 })
+    }
+
+    if ((name as string).toLowerCase() === 'general') {
+      return new NextResponse('Channel name should not be "general"', {
+        status: 400,
+      })
+    }
+
+    const profile = await currentProfile()
+
+    if (!profile) {
+      return new NextResponse('Unauthorized', { status: 401 })
+    }
+
+    const server = await prisma.server.update({
+      where: {
+        id: serverId,
+        members: {
+          some: {
+            profileId: profile.id,
+            role: {
+              in: [MemberRole.ADMIN, MemberRole.MODERATOR],
+            },
+          },
+        },
+      },
+      data: {
+        channels: {
+          update: {
+            where: {
+              id: params.channelId,
+              name: {
+                notIn: ['general', 'General'],
+              },
+            },
+            data: {
+              name,
+              type,
+            },
+          },
+        },
+      },
+    })
+
+    return NextResponse.json(server)
+  } catch (error) {
+    console.log('[CHANNELS_ID_DELETE]', error)
+    return new NextResponse('Internal error', { status: 500 })
+  }
+}
